96a8c2859cdb29669201308fd7e33b4da4e4a528
CassianoPresoto <presoto610@outlook.com>
2024-01-30 19:12:04 -0300

    Add Imgege view in back
    add

--- app/src/main/java/com/example/app_test/MainActivity.kt
+++ app/src/main/java/com/example/app_test/MainActivity.kt
@@ -3,13 +3,22 @@ package com.example.app_test
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.app_test.ui.theme.App_testTheme
 import androidx.compose.ui.unit.sp
 
@@ -22,6 +31,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
+                    GreetingText(message = "Feliz aniversario Sunn!", from = "From Capacho!")
                 }
             }
         }
@@ -32,15 +42,32 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun AppTestCardPreview() {
     App_testTheme {
-        GreetingText(message = "Feliz aniversario sunn!")
+        GreetingImage("Feliz aniversario sunn!", "From Capacho!")
     }
 }
 
 @Composable
-fun GreetingText(message: String, modifier: Modifier = Modifier) {
-    Text(
-        text = message,
-        fontSize = 100.sp,
-        lineHeight = 116.sp,
-    )
+fun GreetingImage(message: String, from: String, modifier: Modifier = Modifier) {
+    val image = painterResource(R.drawable.androidparty)
+    Image(
+         painter = image, contentDescription = null,                                  
+         )
+}
+@Composable
+fun GreetingText(message: String, from: String, modifier: Modifier = Modifier) {
+    Column(modifier = modifier.padding(8.dp), verticalArrangement = Arrangement.Center) {
+        Text(
+            text = message,
+            fontSize = 100.sp,
+            lineHeight = 116.sp,
+            textAlign = TextAlign.Center,
+        )
+        Text(
+            text = from,
+            fontSize = 36.sp,
+            modifier = Modifier
+                .padding(16.dp)
+                .align(alignment = Alignment.End),
+        )
+    }
 }
